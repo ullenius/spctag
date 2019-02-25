e481e2f4c8461b884b1657d608aec6140c90cdf4
Anosh D. Ullenius <anosh@anosh.se>
2019-02-25 18:51:35 +0100

    Adding and updating equals and hashcode methods (they broke since Emulator was replaced by a class)

--- src/main/java/se/anosh/spctag/emulator/factory/Emulator.java
+++ src/main/java/se/anosh/spctag/emulator/factory/Emulator.java
@@ -39,6 +39,42 @@ public abstract class Emulator {
     public String toString() {
         return "Emulator{" + "name=" + name + ", offset= 0x" + Integer.toHexString(code) + '}';
     }
+
+    /**
+	 * Objects will differ is code is different
+	 * even if Name is equals
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + code;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/**
+	 * 
+	 * Objects will differ is code is different
+	 * even if Name is equals
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Emulator other = (Emulator) obj;
+		if (code != other.code)
+			return false;
+		if (name != other.name)
+			return false;
+		return true;
+	}
+    
+    
     
     
     
