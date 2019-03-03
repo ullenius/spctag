ee5eb7658059a7769baaea2cfefa85cca99bbf30
Anosh D. Ullenius <anosh@anosh.se>
2019-03-03 20:22:08 +0100

    Moving constants from dedicated stand-alone file to the class where they are used

--- src/main/java/se/anosh/spctag/dao/SpcFileImplementation.java
+++ src/main/java/se/anosh/spctag/dao/SpcFileImplementation.java
@@ -1,24 +1,5 @@
 package se.anosh.spctag.dao;
 
-import static se.anosh.spctag.Id666Tag.ARTIST_OF_SONG_BINARY_FORMAT_OFFSET;
-import static se.anosh.spctag.Id666Tag.ARTIST_OF_SONG_LENGTH;
-import static se.anosh.spctag.Id666Tag.ARTIST_OF_SONG_TEXT_FORMAT_OFFSET;
-import static se.anosh.spctag.Id666Tag.COMMENTS_LENGTH;
-import static se.anosh.spctag.Id666Tag.COMMENTS_OFFSET;
-import static se.anosh.spctag.Id666Tag.DUMP_DATE_LENGTH;
-import static se.anosh.spctag.Id666Tag.DUMP_DATE_OFFSET;
-import static se.anosh.spctag.Id666Tag.EMULATOR_BINARY_FORMAT_OFFSET;
-import static se.anosh.spctag.Id666Tag.EMULATOR_TEXT_FORMAT_OFFSET;
-import static se.anosh.spctag.Id666Tag.GAME_TITLE_LENGTH;
-import static se.anosh.spctag.Id666Tag.GAME_TITLE_OFFSET;
-import static se.anosh.spctag.Id666Tag.HEADER_CONTAINS_ID666_TAG_OFFSET;
-import static se.anosh.spctag.Id666Tag.HEADER_LENGTH;
-import static se.anosh.spctag.Id666Tag.HEADER_OFFSET;
-import static se.anosh.spctag.Id666Tag.NAME_OF_DUMPER_LENGTH;
-import static se.anosh.spctag.Id666Tag.NAME_OF_DUMPER_OFFSET;
-import static se.anosh.spctag.Id666Tag.SONG_TITLE_LENGTH;
-import static se.anosh.spctag.Id666Tag.SONG_TITLE_OFFSET;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -32,6 +13,36 @@ import se.anosh.spctag.emulator.factory.EmulatorFactory.Type;
 import se.anosh.spctag.emulator.factory.ModernEmulatorFactory;
 
 public class SpcFileImplementation implements SpcDao {
+	
+	//ID666 tag offsets used in SPC-files (Sony SPC-700 sound chip)
+    public static final int HEADER_CONTAINS_ID666_TAG_OFFSET = 0x23; // 1 byte
+    public static final int HEADER_OFFSET = 0;
+    public static final int HEADER_LENGTH = 33;
+
+    public static final int SONG_TITLE_OFFSET = 0x2E;
+    public static final int SONG_TITLE_LENGTH = 32;
+    
+    public static final int GAME_TITLE_OFFSET = 0x4E;
+    public static final int GAME_TITLE_LENGTH = 32;
+    
+    public static final int NAME_OF_DUMPER_OFFSET = 0x6E;
+    public static final int NAME_OF_DUMPER_LENGTH = 16;
+    
+    public static final int COMMENTS_OFFSET = 0x7E;
+    public static final int COMMENTS_LENGTH = 32;
+    
+    public static final int DUMP_DATE_OFFSET = 0x9E;
+    public static final int DUMP_DATE_LENGTH = 11;
+    
+    public static final int ARTIST_OF_SONG_TEXT_FORMAT_OFFSET = 0xB1; // text format
+    public static final int ARTIST_OF_SONG_BINARY_FORMAT_OFFSET = 0xB0;
+    public static final int ARTIST_OF_SONG_LENGTH = 32;
+    
+    public static final int EMULATOR_TEXT_FORMAT_OFFSET = 0xD2;
+    public static final int EMULATOR_BINARY_FORMAT_OFFSET = 0xD1;
+    public static final int EMULATOR_LENGTH = 1;
+	
+	
 
 	private Id666 id666;
 	private Path file;
