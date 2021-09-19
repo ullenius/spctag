667c15b4977949c4ea4a793aecab34ab2dd19df1
Anosh D. Ullenius <anosh@anosh.se>
2021-09-19 10:59:48 +0200

    Refactoring. Creating new enums. Storing data in Xid6 object

--- src/main/java/se/anosh/spctag/dao/Xid6Reader.java
+++ src/main/java/se/anosh/spctag/dao/Xid6Reader.java
@@ -1,7 +1,10 @@
 package se.anosh.spctag.dao;
 
 import org.tinylog.Logger;
+import se.anosh.spctag.domain.Xid6;
+import se.anosh.spctag.domain.Xid6Tag;
 
+import javax.transaction.xa.Xid;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -25,11 +28,13 @@ public class Xid6Reader {
     private static final long XID6_OFFSET = 0x10200L;
 
     static {
-        mappningar.put((byte) 0x1, new Id("Song name", Type.TEXT));
-        mappningar.put((byte) 0x2, new Id("Game name", Type.TEXT));
-        mappningar.put((byte) 0x3, new Id("Artist's name", Type.TEXT));
-        mappningar.put((byte) 0x4, new Id("Dumper's name", Type.TEXT));
-        mappningar.put((byte) 0x5, new Id("Date song was stored", Type.NUMBER));
+        mappningar.put((byte) 0x1, new Id(Xid6Tag.SONG, Type.TEXT));
+        mappningar.put((byte) 0x2, new Id(Xid6Tag.GAME, Type.TEXT));
+        mappningar.put((byte) 0x3, new Id(Xid6Tag.ARTIST, Type.TEXT));
+        mappningar.put((byte) 0x4, new Id(Xid6Tag.DUMPER, Type.TEXT));
+
+
+        mappningar.put((byte) 0x5, new Id("Date song was dumped", Type.NUMBER));
         mappningar.put((byte) 0x6, new Id("Emulator used", Type.DATA));
         mappningar.put((byte) 0x7, new Id("Comments", Type.TEXT));
         mappningar.put((byte) 0x10, new Id("Official Soundtrack Title", Type.TEXT));
@@ -47,8 +52,14 @@ public class Xid6Reader {
         mappningar.put((byte) 0x36, new Id("Mixing (preamp) level", Type.NUMBER));
     }
 
-    final BiConsumer<Id, byte[]> year = (id, data) -> System.out.println("Year: " + toShort(data));
+    private Xid6 xid6 = null;
+
+    final BiConsumer<Id, byte[]> year = (id, data) -> {
+        xid6.setYear(toShort(data));
+        System.out.println("Year: " + xid6.getYear());
+    };
     final BiConsumer<Id, byte[]> muted = (id, data) -> {
+        xid6.setMutedVoices(data[0]);
         System.out.print(id.name + ": ");
         final byte muted = data[0];
         for (int i = 0; i < 8; i++) {
@@ -100,136 +111,136 @@ public class Xid6Reader {
     }
 
     private void mappedVersion() throws IOException {
-
+        var files = listFilesUsingFilesList("");
         Logger.debug("Size of set: {}", files.size());
-        List<Byte> unknownMappings = new LinkedList<>();
-        List<String> unknownMappingfiles = new LinkedList<>();
-
         for (Path spc : files) {
-
             System.out.println("-----------");
             System.out.println("Filename: " + spc.getFileName());
+            parseXid6(spc);
+        }
+    }
 
-            final long fileSize = Files.size(spc);
-            final long xid6Size = fileSize - XID6_OFFSET;
-            final long xid6SizeMinusHeader = xid6Size - 8; // size of header
+    private void parseXid6(Path spc) throws IOException {
+        xid6 = new Xid6();
 
-            if (fileSize <= XID6_OFFSET) {
-                throw new IllegalArgumentException("File too small. Does not contain xid6");
+        final long fileSize = Files.size(spc);
+        final long xid6Size = fileSize - XID6_OFFSET;
+        final long xid6SizeMinusHeader = xid6Size - 8; // size of header
+        if (fileSize <= XID6_OFFSET) {
+            throw new IllegalArgumentException("File too small. Does not contain xid6");
+        }
+
+        var fileChannel = FileChannel.open(spc, StandardOpenOption.READ);
+        fileChannel.position(XID6_OFFSET);
+
+        var buffer = fileChannel.map(
+                FileChannel.MapMode.READ_ONLY, XID6_OFFSET, xid6Size
+        );
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        Logger.debug("Position: {]", buffer.position());
+        Logger.debug("Limit: {]", buffer.limit());
+
+        byte[] magic = new byte[4];
+        buffer.get(magic);
+        ChunkHeader header = new ChunkHeader(magic, buffer.getInt());
+        // workaround if header is broken and actual filesize is less than chunk size in header
+        final int chunkSize = (header.chunkSize > xid6SizeMinusHeader) ? (int) xid6SizeMinusHeader : header.chunkSize;
+        Logger.debug("Chunk size: {}", chunkSize);
+        Logger.debug("Current pos: {}", buffer.position());
+
+        byte[] subChunkArr = new byte[chunkSize]; // chunkSize exclusive header
+        buffer.get(subChunkArr); // contains all sub-chunks, including sub-chunk headers
+        var subChunks = ByteBuffer.wrap(subChunkArr).order(ByteOrder.LITTLE_ENDIAN);
+
+        Logger.debug("Subchunk contents");
+        Logger.debug("-----------");
+        while (subChunks.position() < subChunks.limit()) {
+            Logger.debug("Subchunk header");
+            Logger.debug("Current pos: {}", subChunks.position());
+
+            byte id = subChunks.get();
+            if (!mappningar.containsKey(id)) {
+                break;
             }
+            Id mappatId = mappningar.get(id);
+            Type type = mappatId.type;
+            boolean dataStoredInHeader = subChunks.get() == 0 || type == Type.DATA; // workaround for broken type byte, always read from header
 
-            var fileChannel = FileChannel.open(spc, StandardOpenOption.READ);
-            fileChannel.position(XID6_OFFSET);
+            Logger.debug("Id: 0x{}", toHexString(id));
+            Logger.debug("Mapped id: {}", mappatId);
+            Logger.debug("Data stored in header: {}", dataStoredInHeader);
 
-            var buffer = fileChannel.map(
-                    FileChannel.MapMode.READ_ONLY, XID6_OFFSET, xid6Size
-            );
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            Logger.debug("Position: {]", buffer.position());
-            Logger.debug("Limit: {]", buffer.limit());
+            int size = (dataStoredInHeader)
+                    ? mappatId.type.size()
+                    : subChunks.getShort(); // changes current offset
+            Logger.debug("Size: {}", size);
 
-            byte[] magic = new byte[4];
-            buffer.get(magic);
-            final String magicNumber = "xid6";
-            Logger.debug("Magic number: {}", new String(magic, StandardCharsets.UTF_8)); // FIXME
-            if (!magicNumber.contentEquals(new String(magic, StandardCharsets.UTF_8))) {
+            if (dataStoredInHeader) { // max 2 bytes
+                if (size > 2) {
+                    throw new IllegalStateException("Data stored in header. Yet size is larger than 2 bytes");
+                }
+                byte[] data = new byte[2]; // always allocate 2 bytes
+                subChunks.get(data);
+
+                var func = mappedBehaviourDataStoredInHeader.get(type);
+                func.accept(mappatId, data); // print info
+
+            } else {
+                switch (type) {
+                    case TEXT:
+                        // peek at last byte, workaround for broken tags
+                        if ((size-1) % 4 == 0) {
+                            final int peekPos = size - 1;
+                            final int oldPos = subChunks.position();
+                            byte peek = subChunks.get(oldPos + peekPos);
+                            Logger.debug("Peek byte: {}", peek);
+                            if (mappningar.containsKey(peek)) {
+                                size--;
+                            }
+                        }
+                        int bufsize = size;
+                        while (bufsize % 4 != 0) {
+                            bufsize++;
+                            Logger.debug("Increasing bufsize: {}", bufsize);
+                        }
+                        byte buf[] = new byte[bufsize];
+                        subChunks.get(buf);
+                        String text = new String(buf, StandardCharsets.UTF_8).trim();
+                        System.out.println(mappatId.name + ": " + text);
+                        break;
+                    case INTRO:
+                        if (type.size() == Integer.BYTES) {
+                            xid6.setIntro(subChunks.getInt());
+                            System.out.println("Intro length (seconds): " + xid6.getIntrolength());
+                        }
+                        break;
+                    case NUMBER:
+                        if (type.size() == Integer.BYTES) {
+                            xid6.setNumber(mappatId, subChunks.getInt());
+                            int num = subChunks.getInt();
+                            System.out.println(mappatId.name + " : " +num);
+                        }
+                        break;
+                    default:
+                        throw new IllegalStateException("no mapping for type: " + type);
+                }
+            }
+        }
+        fileChannel.close();
+    }
+
+    private static class ChunkHeader {
+        private static final String MAGIC_NUMBER = "xid6";
+        private final int chunkSize;
+        ChunkHeader(byte[] magicNumber, int size) {
+            String actual = new String(magicNumber, StandardCharsets.UTF_8);
+            if (!MAGIC_NUMBER.contentEquals(actual)) {
                 throw new IllegalArgumentException("invalid magic number");
             }
-
-            // workaround if header is broken and actual filesize is less than chunk size in header
-            int tmp = buffer.getInt();
-            final int chunkSize = (tmp > xid6SizeMinusHeader) ? (int) xid6SizeMinusHeader : tmp;
-
-            Logger.debug("Chunk size: {}", chunkSize);
-            Logger.debug("Current pos: {}", buffer.position());
-
-            byte[] subChunkArr = new byte[chunkSize]; // chunkSize exclusive header
-            buffer.get(subChunkArr); // contains all sub-chunks, including sub-chunk headers
-            var subChunks = ByteBuffer.wrap(subChunkArr).order(ByteOrder.LITTLE_ENDIAN); // FIXME felaktigt namn, är hela subchunken
-
-            Logger.debug("Subchunk contents");
-            Logger.debug("-----------");
-            while (subChunks.position() < subChunks.limit()) {
-                Logger.debug("Subchunk header");
-                Logger.debug("Current pos: {}", subChunks.position());
-
-                byte id = subChunks.get();
-                if (!mappningar.containsKey(id)) {
-                    unknownMappings.add(id); // debug
-                    unknownMappingfiles.add(spc.getFileName().toString()); // debug
-                    break;
-                }
-                Id mappatId = mappningar.get(id);
-                Type type = mappatId.type;
-
-                boolean dataStoredInHeader = subChunks.get() == 0 || type == Type.DATA; // workaround for broken type byte, always read from header
-
-                Logger.debug("Id: 0x{}", toHexString(id));
-                Logger.debug("Mapped id: {}", mappatId);
-                Logger.debug("Data stored in header: {}", dataStoredInHeader);
-
-                int size = (dataStoredInHeader)
-                        ? mappatId.type.size()
-                        : subChunks.getShort(); // changes current offset
-                Logger.debug("Size: {}", size);
-
-                if (dataStoredInHeader) { // max 2 bytes
-                    if (size > 2) {
-                        throw new IllegalStateException("Data stored in header. Yet size is larger than 2 bytes");
-                    }
-                    byte[] data = new byte[2]; // always allocate 2 bytes
-                    subChunks.get(data);
-
-                    var func = mappedBehaviourDataStoredInHeader.get(type);
-                    func.accept(mappatId, data); // print info
-
-                } else {
-                    switch (type) {
-                        case TEXT:
-                            // peek at last byte, workaround for broken tags
-                            if ((size-1) % 4 == 0) {
-                                final int peekPos = size - 1;
-                                final int oldPos = subChunks.position();
-                                byte peek = subChunks.get(oldPos + peekPos);
-                                Logger.debug("Peek byte: {}", peek);
-                                if (mappningar.containsKey(peek)) {
-                                    size--;
-                                }
-                            }
-                            int bufsize = size;
-                            while (bufsize % 4 != 0) {
-                                bufsize++;
-                                Logger.debug("Increasing bufsize: {}", bufsize);
-                            }
-                            byte buf[] = new byte[bufsize];
-                            subChunks.get(buf);
-                            String text = new String(buf, StandardCharsets.UTF_8).trim();
-                            System.out.println(mappatId.name + ": " + text);
-                            break;
-                        case INTRO:
-                            if (type.size() == Integer.BYTES) {
-                                int num = subChunks.getInt();
-                                Logger.debug("Number: " + num);
-                                System.out.println("Intro length (seconds): " + num / INTRO_LENGTH_DIVISOR);
-                            }
-                            break;
-                        case NUMBER:
-                            if (type.size() == Integer.BYTES) {
-                                int num = subChunks.getInt();
-                                System.out.println(mappatId.name + " : " +num);
-                            }
-                            break;
-                        default:
-                            throw new IllegalStateException("no mapping for type: " + type);
-                    }
-                }
-            }
-            fileChannel.close();
+            this.chunkSize = size;
         }
-        Logger.debug("Unknown mappings: {}", unknownMappings);
-        Collections.sort(unknownMappingfiles);
-        Logger.debug("Files with unkown mappings {}", unknownMappingfiles);
     }
+
 
     private short toShort(byte buf[]) { // little endian
         return (short) (
@@ -237,18 +248,18 @@ public class Xid6Reader {
     }
 
     private static final class Id {
-        Id(String name, Type type) {
-            this.name = name;
+        Id(Xid6Tag tag, Type type) {
+            this.tag = tag;
             this.type = type;
         }
 
-        final String name;
+        final Xid6Tag tag;
         final Type type;
 
         @Override
         public String toString() {
             return "Id{" +
-                    "name='" + name + '\'' +
+                    "name='" + tag + '\'' +
                     ", type=" + type +
                     '}';
         }
--- src/main/java/se/anosh/spctag/domain/Xid6.java
+++ src/main/java/se/anosh/spctag/domain/Xid6.java
@@ -1,12 +1,22 @@
 package se.anosh.spctag.domain;
 
+import se.anosh.spctag.dao.Xid6Reader;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Year;
+
 public class Xid6 {
+
+    private static final double INTRO_LENGTH_DIVISOR = 64_0000.0;
 
     private String song;
     private String game;
     private String artist;
     private String dumper;
-    private int date;
+    private LocalDate dumped; // date song was dumped
+    private Year year;
     private byte emulator; // fixme unsigned
     private String comments;
     private byte ostDisc;
@@ -19,6 +29,150 @@ public class Xid6 {
     private byte mutedVoices; // fixme unsigned
     private byte loop; // fixme
     private byte mixingLevel; // fixme
+    private int introLength;
 
+    private static final SimpleDateFormat dumpedDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public void setDate(int date) throws ParseException {
+        dumpedDateFormat.parse(Integer.toString(date));
+    }
+
+    public void setNumber(Object foo, int num) {
+
+    }
+
+
+    public void setIntro(int length) {
+        this.introLength = length;
+    }
+
+    public double getIntrolength() {
+        return introLength / INTRO_LENGTH_DIVISOR;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getDumper() {
+        return dumper;
+    }
+
+    public void setDumper(String dumper) {
+        this.dumper = dumper;
+    }
+
+
+    public byte getEmulator() {
+        return emulator;
+    }
+
+    public void setEmulator(byte emulator) {
+        this.emulator = emulator;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public byte getOstDisc() {
+        return ostDisc;
+    }
+
+    public void setOstDisc(byte ostDisc) {
+        this.ostDisc = ostDisc;
+    }
+
+    public char[] getOstTrack() {
+        return ostTrack;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getLoopLength() {
+        return loopLength;
+    }
+
+    public void setLoopLength(int loopLength) {
+        this.loopLength = loopLength;
+    }
+
+    public int getEndLength() {
+        return endLength;
+    }
+
+    public void setEndLength(int endLength) {
+        this.endLength = endLength;
+    }
+
+    public int getFadeLength() {
+        return fadeLength;
+    }
+
+    public void setFadeLength(int fadeLength) {
+        this.fadeLength = fadeLength;
+    }
+
+    public byte getMutedVoices() {
+        return mutedVoices;
+    }
+
+    public void setMutedVoices(byte mutedVoices) {
+        this.mutedVoices = mutedVoices;
+    }
+
+    public byte getLoop() {
+        return loop;
+    }
+
+    public void setLoop(byte loop) {
+        this.loop = loop;
+    }
+
+    public byte getMixingLevel() {
+        return mixingLevel;
+    }
+
+    public void setMixingLevel(byte mixingLevel) {
+        this.mixingLevel = mixingLevel;
+    }
+
+    public void setYear(int year) {
+        this.year = Year.of(year);
+    }
+
+    public Year getYear() {
+        return year;
+    }
 
 }
