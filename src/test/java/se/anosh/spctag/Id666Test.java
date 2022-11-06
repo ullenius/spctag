c5e807fe8729ecdad9806d9bdba796f1527bcbf0
Anosh D. Ullenius <anosh@anosh.se>
2022-11-06 18:00:02 +0100

    Move all domain class tests to one class

--- src/test/java/se/anosh/spctag/Id666Test.java
+++ src/test/java/se/anosh/spctag/Id666Test.java
@@ -2,12 +2,21 @@ package se.anosh.spctag;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.tinylog.Logger;
+import se.anosh.spctag.dao.SpcDao;
+import se.anosh.spctag.dao.SpcFile;
 import se.anosh.spctag.domain.Id666;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.LinkedList;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static se.anosh.spctag.TestModelWithData.BINARY_SPC;
+import static se.anosh.spctag.TestModelWithData.TEXT_SPC;
 
 public class Id666Test {
 
@@ -33,6 +42,77 @@ public class Id666Test {
         uut.setDateDumpWasCreated(earlyDate);
         assertEquals(expected, uut.getDateDumpWasCreated());
     }
+
+    @Test
+    public void hashCodeAndEqualsIdentical() throws IOException {
+        SpcFile textSpc = new SpcFile(TEXT_SPC);
+        SpcFile textSpc2 = new SpcFile(TEXT_SPC);
+
+        assertNotSame(textSpc, textSpc2);
+        final Id666 first = textSpc.read();
+        final Id666 second = textSpc2.read();
+        assertEquals(first.hashCode(), second.hashCode());
+        assertEquals(first, second);
+    }
+
+    @Test
+    public void differentHashcodes() throws IOException {
+        SpcFile binary = new SpcFile(BINARY_SPC);
+        Id666 binaryTags = binary.read();
+
+        assertNotSame(binaryTags, uut);
+        assertNotEquals(binaryTags, uut);
+        assertNotEquals(binaryTags.hashCode(), uut.hashCode());
+    }
+
+    @Test
+    public void nonEqualObjects() throws IOException {
+        SpcFile binarySpc = new SpcFile(BINARY_SPC);
+        SpcFile textSpc = new SpcFile(TEXT_SPC);
+
+        Id666 binaryTags = binarySpc.read();
+        Id666 textTags = textSpc.read();
+        assertNotEquals(binaryTags, textTags);
+    }
+
+    @Test
+    public void sortingWorks() throws IOException {
+        final SpcFile otherFile = new SpcFile(BINARY_SPC);
+        final Id666 other = otherFile.read();
+
+        List<Id666> myList = new LinkedList<>();
+        myList.add(other);
+        myList.add(uut);
+        myList.add(other);
+        myList.add(uut);
+
+        myList.sort(null);
+        myList.forEach(Logger::debug);
+        assertEquals(uut, myList.get(0));
+        assertEquals(uut, myList.get(1));
+        assertEquals(other, myList.get(2));
+        assertEquals(other, myList.get(3));
+    }
+
+    @Test
+    public void comparatorAllowsNullValues() throws IOException {
+        final SpcDao binary = new SpcFile(BINARY_SPC);
+        final Id666 binaryTags = binary.read();
+        final Id666 emptyTags = new Id666();
+
+        List<Id666> myList = new LinkedList<>();
+        myList.add(binaryTags);
+        myList.add(emptyTags);
+        myList.sort(null);
+
+        assertNull(myList.get(0).getSongTitle());
+        assertNull(myList.get(0).getGameTitle());
+        assertNull(myList.get(0).getArtist());
+        assertNotNull(myList.get(1).getGameTitle());
+        assertNotNull(myList.get(1).getArtist());
+        assertNotNull(myList.get(1).getSongTitle());
+    }
+
 
 
 }
