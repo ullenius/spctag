2b0015e52d2aac171c92635f3ea37d453cac4643
Anosh D. Ullenius <anosh@anosh.se>
2022-11-13 17:44:39 +0100

    Refactor tests. Use data providers

--- src/test/java/se/anosh/spctag/emulator/factory/JapaneseEmulatorTest.java
+++ src/test/java/se/anosh/spctag/emulator/factory/JapaneseEmulatorTest.java
@@ -1,38 +1,28 @@
 package se.anosh.spctag.emulator.factory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static se.anosh.spctag.emulator.factory.JapaneseEmulator.*;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import se.anosh.spctag.emulator.factory.EmulatorFactory.Type;
 
 public class JapaneseEmulatorTest {
-    
+
     private EmulatorFactory factory;
-    
-    // Magic number constants. Used by test-classes in this package
-    // They don't match any emulator code. Used for testing
-    // sad path
+
     static final int INVALID_POSITIVE_NUMBER = 1337;
     static final int INVALID_NEGATIVE_NUMBER = -15;
-    
+
     @BeforeEach
     public void setup() {
-    	factory = new ModernEmulatorFactory();
+        factory = new ModernEmulatorFactory();
     }
-    
-    /**
-     * 
-     * Tests that the method returns the correct 
-     * enumeration based on the code provided
-     */
+
     @ParameterizedTest
-    @MethodSource("provideStringsForIsBlank")
-    public void testValidJapaneseEmulatorCodes(final int code, final Name expected) {
+    @MethodSource("validCodes")
+    public void japaneseEmulatorCodeMappings(final int code, final Name expected) {
         final Emulator emulator = factory.orderEmulator(code, Type.JAPANESE);
         final Name actual = emulator.getName();
         assertEquals(expected, actual);
@@ -56,37 +46,36 @@ public class JapaneseEmulatorTest {
                 {ZSNES_W_TEXT, Name.ZSNES_W},
                 {ZSNES_W_BINARY, Name.ZSNES_W},
 
-                { SNES9X_TEXT, Name.Snes9x},
-                { SNES9X_BINARY, Name.Snes9x},
+                {SNES9X_TEXT, Name.Snes9x},
+                {SNES9X_BINARY, Name.Snes9x},
 
-                { SNES9XPP_TEXT, Name.Snes9xpp},
-                { SNES9XPP_BINARY, Name.Snes9xpp},
+                {SNES9XPP_TEXT, Name.Snes9xpp},
+                {SNES9XPP_BINARY, Name.Snes9xpp},
 
-                { SNESGT_TEXT, Name.SNESGT},
-                { SNESGT_BINARY, Name.SNESGT},
+                {SNESGT_TEXT, Name.SNESGT},
+                {SNESGT_BINARY, Name.SNESGT},
 
-                { OTHER_TEXT, Name.Other},
-                { OTHER_BINARY, Name.Other},
+                {OTHER_TEXT, Name.Other},
+                {OTHER_BINARY, Name.Other},
 
-                { UNKNOWN_TEXT, Name.Unknown},
-                { UNKNOWN_BINARY, Name.Unknown},
+                {UNKNOWN_TEXT, Name.Unknown},
+                {UNKNOWN_BINARY, Name.Unknown},
         };
     }
-    
-    @Test
-    public void testInvalidJapaneseEmulatorCodes() {
-        Emulator result;
-        result = factory.orderEmulator(SNES9X_TEXT, Type.JAPANESE);
-        assertNotEquals(Name.ZSNES,result.getName());
-        
-        // sad path testing
-        result = factory.orderEmulator(INVALID_POSITIVE_NUMBER, Type.JAPANESE);
-        assertEquals(Name.Unknown,result.getName()); // unknown is default
-        
-        result = factory.orderEmulator(INVALID_NEGATIVE_NUMBER, Type.JAPANESE);
-        assertEquals(Name.Unknown,result.getName());
-        
+
+    @ParameterizedTest
+    @MethodSource("invalidCodes")
+    public void testInvalidJapaneseEmulatorCodes(final int code) {
+        final Emulator emulator = factory.orderEmulator(code, Type.JAPANESE);
+        final Name actual = emulator.getName();
+        assertEquals(Name.Unknown, actual);
     }
-    
-    
+
+    private static Object[][] invalidCodes() {
+        return new Object[][]{
+                {INVALID_POSITIVE_NUMBER},
+                {INVALID_NEGATIVE_NUMBER},
+        };
+    }
+
 }
