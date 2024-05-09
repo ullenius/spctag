82f19a104cc453f23e16c16fc7614662f269d630
Anosh D. Ullenius <anosh@anosh.se>
2024-05-09 20:11:28 +0200

    Refactoring. Remove unused imports

--- src/main/java/se/anosh/spctag/Xid6Util.java
+++ src/main/java/se/anosh/spctag/Xid6Util.java
@@ -10,10 +10,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
